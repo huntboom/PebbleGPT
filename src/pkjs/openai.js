56af6c0e859d3fbd2cc3032f5506db905d9b9e70
Ron Smeral <ron.smeral@gmail.com>
2024-01-25 01:24:15 +0100

    enable conversations

--- src/pkjs/openai.js
+++ src/pkjs/openai.js
@@ -6,6 +6,9 @@ var {
   MODEL,
 } = require("./config");
 
+// hold received responses for the duration of the chat session
+var messages = [];
+
 function makeOpenaiRequest(prompt, onResponse, onError) {
   var config = getConfig();
 
@@ -26,7 +29,9 @@ function makeOpenaiRequest(prompt, onResponse, onError) {
       return;
     }
 
-    onResponse(responseBody.choices[0].message.content);
+    var chatCompletion = responseBody.choices[0].message.content;
+    messages.push({ role: "assistant", content: chatCompletion });
+    onResponse(chatCompletion);
   };
 
   // Send the request
@@ -34,11 +39,11 @@ function makeOpenaiRequest(prompt, onResponse, onError) {
   request.setRequestHeader("Content-Type", "application/json");
   request.setRequestHeader("Authorization", "Bearer " + config[API_KEY]);
 
-  var messages = [{ role: "user", content: prompt }];
-
-  if (config[SYSTEM_PROMPT]) {
-    messages.unshift({ role: "system", content: config[SYSTEM_PROMPT] });
+  if (messages.length === 0 && config[SYSTEM_PROMPT]) {
+    messages.push({ role: "system", content: config[SYSTEM_PROMPT] });
   }
+
+  messages.push({ role: "user", content: prompt });
 
   var requestBody = JSON.stringify({
     model: config[MODEL],
